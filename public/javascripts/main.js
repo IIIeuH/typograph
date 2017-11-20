93a400e4b28347f7d4ac24a8ef50456a77d2b776
IIIeuH <shein.ev@gmail.com>
2017-11-21 00:58:21 +0300

    refactor

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,4 +5,26 @@ $(function(){
     $('#select2').select2();
     $('#select3').select2();
     $('#select4').select2();
+    //поиск в pasports
+    var data = $('.passport-date');
+    var id   = $('.passport-id');
+    var strData = $('#searchDate');
+    var strId = $('#searchId');
+    strData.keyup(function(){
+        searchPassports($(this).val(), data);
+    });
+    strId.keyup(function(){
+        searchPassports($(this).val(), id);
+    });
 });
+
+function searchPassports(str, field){
+    $.each(field, function () {
+        var tr = $(this).parent();
+        if(!!~$(this).text().indexOf(str) || str === ''){
+            tr.show();
+        }else{
+            tr.hide();
+        }
+    })
+}
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('manager', { title: 'Express' });
+  res.render('manager', { title: 'Manager' });
+});
+
+router.get('/passport', function(req, res, next) {
+    res.render('passport', { title: 'Passport form' });
+});
+
+
+router.get('/passports', function(req, res, next) {
+    res.render('passports', { title: 'All passports' });
 });
 
 module.exports = router;
