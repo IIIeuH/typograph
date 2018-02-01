76a74bdf491fe343408ac4a97116ab054cefcbbb
IIIeuH <shein.ev@gmail.com>
2018-02-01 21:25:09 +0300

    auth

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const ctrl   = require('../ctrl');
 const managerRoutes = require('./manager/index');
+const expressKerberos = require('express-kerberos');
 
 //ajax
 router.route('/getCollection')
@@ -10,11 +11,11 @@ router.get('/login', (req, res) => {
     res.render('login', { title: 'login' });
 });
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Manager' });
-});
 
+/* GET home page. */
+router.get('/', expressKerberos.default(), function(req, res, next) {
+  res.render('index', { title: 'Manager', user: req.auth});
+});
 router.use('/manager', managerRoutes);
 
 
